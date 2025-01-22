82aa7f0bed0b0d445157454e0301382cc2099318
Andrey Dolganov <dolganov2002@mail.ru>
2025-01-22 23:45:25 +0300

    step three, create database

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,12 +1,12 @@
 import os
-
+import psycopg2
 from dotenv import load_dotenv
 from flask import Flask, render_template
 
 load_dotenv()
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-
+app.config['DATABASE_URL'] = os.getenv('DATABASE_URL')
 
 @app.route("/")
 def hello_world():
