f734b864ff37a843d2f6058ff3ba5bfb72e9f5d1
Andrey Dolganov <dolganov2002@mail.ru>
2025-01-23 23:57:24 +0300

    add html files

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -19,6 +19,7 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 conn = psycopg2.connect(os.getenv('DATABASE_URL'))
 repo = UrlRepository(conn)
 
+
 @app.route("/")
 def home_page():
     messages = get_flashed_messages(with_categories=True)
@@ -31,8 +32,25 @@ def url_post():
     if validators.url(url_data['url']) is True:
         repo.save(url_data)
         flash('Страница успешно добавлена', 'success')
-        return redirect(url_for('url_show'), code=302)
+        return redirect(url_for('home_page'), code=302)
     else:
-        return render_template(
-            url_for('home_page')
-        )
+        return render_template(url_for('home_page'))
+
+
+@app.route('/urls/<id>')
+def users_show(id):
+    url = repo.find(id)
+    return render_template(
+        'show.html',
+        url=url,
+    )
+
+@app.route('/urls')
+def urls_index():
+    messages = get_flashed_messages(with_categories=True)
+    urls = repo.get_content()
+    return render_template(
+        'index.html',
+        urls=urls,
+        messages=messages
+    )
--- page_analyzer/url_repository.py
+++ page_analyzer/url_repository.py
@@ -1,4 +1,3 @@
-import psycopg2
 from psycopg2.extras import RealDictCursor
 
 
@@ -6,3 +5,12 @@ class UrlRepository:
     def __init__(self, conn):
         self.conn = conn
 
+    def find(self, id):
+        with self.conn.cursor(cursor_factory=RealDictCursor) as cur:
+            cur.execute("SELECT * FROM urls WHERE id = %s", (id,))
+            return cur.fetchone()
+
+    def get_content(self):
+        with self.conn.cursor(cursor_factory=RealDictCursor) as cur:
+            cur.execute("SELECT * FROM urls")
+            return cur.fetchall()
