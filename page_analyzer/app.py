b0d1b2851f9f4ba94e31835e174fe46611ebfa81
Andrey Dolganov <dolganov2002@mail.ru>
2025-01-25 12:11:42 +0300

    end of third step

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -2,6 +2,7 @@ import os
 import psycopg2
 import validators
 from dotenv import load_dotenv
+from urllib.parse import urlparse
 from flask import (
     get_flashed_messages,
     flash,
@@ -26,24 +27,39 @@ def home_page():
     return render_template('home.html', messages=messages)
 
 
-@app.post('/')
-def url_post():
-    url_data = request.form.to_dict()
-    if validators.url(url_data['url']) is True:
-        repo.save(url_data)
-        flash('Страница успешно добавлена', 'success')
-        return redirect(url_for('home_page'), code=302)
-    else:
-        return render_template(url_for('home_page'))
+@app.post('/urls')
+def urls_post():
+    url_data = request.form.get('url')
+    if validators.url(url_data) is not True:
+        flash('Некорректный URL', 'error')
+        messages = get_flashed_messages(with_categories=True)
+        return render_template("home.html", url=url_data, messages=messages)
+
+    parsed_url = urlparse(url_data)
+    new_url = f"{parsed_url.scheme}://{parsed_url.netloc}"
+
+    if repo.find_name(new_url):
+        url_id = repo.find_name(new_url)['id']
+        flash('Страница уже существует', 'info')
+        return redirect(url_for('urls_show', id=url_id), code=302)
+
+    url_id = repo.save(new_url)
+    flash('Страница успешно добавлена', 'success')
+    return redirect(url_for('urls_show', id=url_id), code=302)
+
+
 
 
 @app.route('/urls/<id>')
-def users_show(id):
-    url = repo.find(id)
+def urls_show(id):
+    messages = get_flashed_messages(with_categories=True)
+    url = repo.find_id(id)
     return render_template(
         'show.html',
         url=url,
+        messages=messages
     )
+
 
 @app.route('/urls')
 def urls_index():
--- page_analyzer/url_repository.py
+++ page_analyzer/url_repository.py
@@ -1,16 +1,32 @@
 from psycopg2.extras import RealDictCursor
+from datetime import date
 
 
 class UrlRepository:
     def __init__(self, conn):
         self.conn = conn
 
-    def find(self, id):
+    def find_id(self, id):
         with self.conn.cursor(cursor_factory=RealDictCursor) as cur:
             cur.execute("SELECT * FROM urls WHERE id = %s", (id,))
             return cur.fetchone()
 
     def get_content(self):
         with self.conn.cursor(cursor_factory=RealDictCursor) as cur:
-            cur.execute("SELECT * FROM urls")
+            cur.execute("SELECT * FROM urls ORDER BY created_at DESC, id DESC")
             return cur.fetchall()
+
+    def save(self, url_data):
+        with self.conn.cursor() as cur:
+            cur.execute(
+                "INSERT INTO urls (name, created_at) VALUES (%s, %s) RETURNING id",
+                (url_data, date.today())
+            )
+            id = cur.fetchone()[0]
+        self.conn.commit()
+        return id
+
+    def find_name(self, name):
+        with self.conn.cursor(cursor_factory=RealDictCursor) as cur:
+            cur.execute("SELECT * FROM urls WHERE name = %s", (name,))
+            return cur.fetchone()
