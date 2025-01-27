986bedee041fb84b9cac27ce8b599175006b8a75
DolAndd <dolganov2002@mail.ru>
2025-01-27 18:08:00 +0300

    create table url_checks

--- database.sql
+++ database.sql
@@ -3,3 +3,13 @@ CREATE TABLE IF NOT EXISTS urls (
     name VARCHAR(255) NOT NULL,
     created_at DATE NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS url_checks (
+    id SERIAL PRIMARY KEY,
+    url_id integer NOT NULL,
+    status_code integer,
+    h1 VARCHAR,
+    title VARCHAR,
+    description VARCHAR,
+    created_at DATE NOT NULL
+);
